913e51e8175e36c7e8638985f11ce28546c5c475
Ravern Koh <ravernkoh@gmail.com>
2019-02-15 22:33:37 +0800

    Add third layer for utilities

--- config.rb
+++ config.rb
@@ -10,6 +10,11 @@ keyboard title: "Custom mappings with layers" do
       to layer: [1, :once]
     end
 
+    mapping do
+      from keys: ["n", "m"]
+      to layer: [2, :once]
+    end
+
     # Temporary to learn backspace.
     mapping do
       from key: ["left_control", "h"]
@@ -34,14 +39,27 @@ keyboard title: "Custom mappings with layers" do
       ["i", "8"],
       ["o", "9"],
       ["p", "0"],
-      ["open_bracket", "hyphen"],
-      ["close_bracket", "equal_sign"],
+      ["a", ""],
+      ["s", ""],
+      ["d", ""],
+      ["f", ""],
+      ["g", ""],
       ["h", "left_arrow"],
       ["j", "down_arrow"],
       ["k", "up_arrow"],
       ["l", "right_arrow"],
+      ["z", ""],
+      ["x", ""],
+      ["c", ""],
+      ["v", ""],
+      ["b", ""],
+      ["n", ""],
+      ["m", ""],
+      ["open_bracket", "hyphen"],
+      ["close_bracket", "equal_sign"],
       ["semicolon", "backslash"],
       ["quote", "delete_or_backspace"],
+      ["spacebar", "escape"],
     ].each do |pair|
       mapping do
         from key: pair[0]
@@ -53,6 +71,31 @@ keyboard title: "Custom mappings with layers" do
     mapping do
       from key: ["left_control", "h"]
       to layer: 1
+    end
+  end
+
+  layer desc: "Utilities like changing desktop, etc." do
+    mapping do
+      from keys: ["n", "m"]
+      to layer: 0
+    end
+
+    [
+      ["q", ["left_control", "1"]],
+      ["w", ["left_control", "2"]],
+      ["e", ["left_control", "3"]],
+      ["r", ["left_control", "4"]],
+      ["t", ["left_control", "5"]],
+      ["y", ["left_control", "6"]],
+      ["u", ["left_control", "7"]],
+      ["i", ["left_control", "8"]],
+      ["o", ["left_control", "9"]],
+      ["p", ["left_control", "0"]],
+    ].each do |pair|
+      mapping do
+        from key: pair[0]
+        to key: pair[1]
+      end
     end
   end
 end
