5cb132c7ff732beee7daf70430acbde788a84887
Ravern Koh <ravernkoh@gmail.com>
2019-02-15 18:13:00 +0800

    Allow basic numbers to work

--- config.rb
+++ config.rb
@@ -1,33 +1,33 @@
-keyboard do
-  layer :normal, title: "Basic typing and most punctuation" do
+keyboard title: "Custom mappings with layers" do
+  layer desc: "Basic typing and most punctuation" do
     mapping do
-      from key: ['a', 's']
-      to mode: :numbers
+      from keys: ["a", "s"]
+      to mode: 1
     end
-    
+
     mapping do
-      from key: ['d', 'f']
-      to mode: [:numbers, :once]
+      from keys: ["d", "f"]
+      to mode: [1, :once]
     end
   end
 
-  layer :numbers, title: "Numbers and some additional punctutation" do
+  layer desc: "Numbers and some additional punctutation" do
     mapping do
-      from key: ['a', 's']
-      to mode: :normal
+      from keys: ["a", "s"]
+      to mode: 0
     end
 
     [
-      ['q', '1'],
-      ['w', '2'],
-      ['e', '3'],
-      ['r', '4'],
-      ['t', '5'],
-      ['y', '6'],
-      ['u', '7'],
-      ['i', '8'],
-      ['o', '8'],
-      ['p', '0'],
+      ["q", "1"],
+      ["w", "2"],
+      ["e", "3"],
+      ["r", "4"],
+      ["t", "5"],
+      ["y", "6"],
+      ["u", "7"],
+      ["i", "8"],
+      ["o", "8"],
+      ["p", "0"],
     ].each do |pair|
       mapping do
         from key: pair[0]
--- index.rb
+++ index.rb
@@ -1,10 +1,120 @@
 class Mapping
-  attr_accessor :desc
+  def from(from = {})
+    @from = from
+  end
+
+  def to(to = {})
+    @to = to
+  end
+
+  def build(layer)
+    {
+      type: "basic",
+      from: build_from,
+      conditions: build_conditions(layer),
+      to: build_to,
+    }
+  end
+
+  private
+
+  def build_from
+    if @from[:key]
+      build_from_key(@from[:key])
+    elsif @from[:keys]
+      { simultaneous: @from[:keys].map { |key| build_from_key(key) } }
+    end
+  end
+
+  def build_from_key(key)
+    if key.kind_of?(Array)
+      {
+        key_code: key[1],
+        modifiers: { mandatory: build_from_key_mods(key[0]) },
+      }
+    else
+      {
+        key_code: key,
+        modifiers: { optional: ["any"] },
+      }
+    end
+  end
+
+  def build_from_key_mods(mods)
+    if mods.kind_of?(Array)
+      mods
+    else
+      [mods]
+    end
+  end
+
+  def build_conditions(layer)
+    [
+      {
+        type: "variable_if",
+        name: "custom_mode",
+        value: layer,
+      },
+    ]
+  end
+
+  def build_to
+    if @to[:key]
+      [build_to_key(@to[:key])]
+    elsif @to[:keys]
+      @to[:keys].map { |key| build_to_key(key) }
+    elsif @to[:mode]
+      [build_to_mode(@to[:mode])]
+    end
+  end
+
+  def build_to_key(key)
+    if key.kind_of?(Array)
+      {
+        key_code: key[1],
+        modifiers: build_to_key_mods(key[0]),
+      }
+    else
+      {
+        key_code: key,
+        modifiers: [],
+      }
+    end
+  end
+
+  def build_to_key_mods(mods)
+    if mods.kind_of?(Array)
+      mods
+    else
+      [mods]
+    end
+  end
+
+  def build_to_mode(mode)
+    if mode.kind_of?(Array)
+      if mode[1] == :once
+        {
+          set_variable: {
+            name: "custom_mode",
+            value: mode,
+          },
+        }
+      else
+        raise ArgumentError()
+      end
+    else
+      {
+        set_variable: {
+          name: "custom_mode",
+          value: mode,
+        },
+      }
+    end
+  end
 end
 
 class Layer
-  attr_accessor :name
-  attr_accessor :title
+  attr_accessor :desc
 
   def initialize
     @mappings = []
@@ -12,7 +122,6 @@ class Layer
 
   def mapping(desc: nil, &block)
     mapping = Mapping.new
-    mapping.desc = desc
     mapping.instance_eval(&block)
 
     @mappings << mapping
@@ -20,23 +129,24 @@ class Layer
     mapping
   end
 
-  def build
+  def build(layer)
     {
-      title: @title,
-      rules: @mappings.map { |mapping| mapping.build(self) },
+      description: @desc,
+      manipulators: @mappings.map { |mapping| mapping.build(layer) },
     }
   end
 end
 
 class Keyboard
+  attr_accessor :title
+
   def initialize
     @layers = []
   end
 
-  def layer(name, title: nil, &block)
+  def layer(desc: nil, &block)
     layer = Layer.new
-    layer.name = name.to_s
-    layer.title = title || name.to_s
+    layer.desc = desc || "Custom layer"
     layer.instance_eval(&block)
 
     @layers << layer
@@ -45,25 +155,54 @@ class Keyboard
   end
 
   def build
-    @layers.map do |layer|
-      layer.build
-    end
+    {
+      title: @title,
+      rules: @layers.each_with_index.map do |layer, i|
+        layer.build(i)
+      end + [RESET_LAYER],
+    }
   end
 end
 
 KEYBOARD = Keyboard.new
 
-def keyboard(&block)
+RESET_LAYER = {
+  description: "Reset to normal",
+  manipulators: [
+    {
+      type: "basic",
+      from: {
+        key_code: "r",
+        modifiers: {
+          mandatory: [
+            "left_command",
+            "left_option",
+          ],
+        },
+      },
+      to: [
+        {
+          set_variable: {
+            name: "custom_mode",
+            value: 0,
+          },
+        },
+      ],
+    },
+  ],
+}
+
+def keyboard(title: nil, &block)
+  KEYBOARD.title = title || "Custom rules"
   KEYBOARD.instance_eval(&block)
 end
 
-require './config'
+require "./config"
+require "json"
 
-DEFAULT_PATH = '~/.config/karabiner/assets/complex_modifications/'
+DEFAULT_PATH = ENV["HOME"] + "/.config/karabiner/assets/complex_modifications/custom.json"
 
-KEYBOARD.build.each do |layer|
-  path = (ARGS[0] || DEFAULT_PATH) + layer.title
-  File.open(path, 'w') do |f|
-    f.puts layer.to_json
-  end
+path = ARGV[0] || DEFAULT_PATH
+File.open(path, "w") do |f|
+  f.puts KEYBOARD.build.to_json
 end
